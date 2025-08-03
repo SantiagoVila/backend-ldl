95c5ae87c4816f4267ea1a92fde1d18874d650cf
SantiagoVila <vilasantiago16@gmail.com>
2025-08-03 19:11:51 -0300

    PRUEBA COPA 7

--- src/routes/copas.routes.js
+++ src/routes/copas.routes.js
@@ -8,18 +8,22 @@ const {
     obtenerCopas, 
     obtenerCopaPorId,
     obtenerDetallesPublicosCopa,
-    borrarCopa // ✅ Se importa la nueva función
+    borrarCopa,
+    obtenerCopasPublico
 } = require("../controllers/copas.controller");
 const verificarToken = require("../middleware.js/auth.middleware");
 const verificarRol = require("../middleware.js/verifyRole");
 
 // --- RUTAS PÚBLICAS ---
+// ✅ Estas rutas no usan 'verificarToken' y son accesibles para todos.
+router.get('/publico', obtenerCopasPublico);
 router.get('/publico/:id/detalles', obtenerDetallesPublicosCopa);
 
 // --- RUTAS DE ADMIN ---
+// ✅ 'verificarToken' y 'verificarRol' se aplican individualmente a cada ruta protegida.
 router.get('/', verificarToken, verificarRol("admin"), obtenerCopas);
 router.get('/:id', verificarToken, verificarRol("admin"), obtenerCopaPorId);
 router.post('/', verificarToken, verificarRol("admin"), crearCopa);
-router.delete('/:id', verificarToken, verificarRol("admin"), borrarCopa); // ✅ NUEVA RUTA
+router.delete('/:id', verificarToken, verificarRol("admin"), borrarCopa);
 
 module.exports = router;
